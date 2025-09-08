e78d81022515be931e12433db08ce05a34897458
santiagon1820 <230399363+santiagon1820@users.noreply.github.com>
2025-09-08 14:38:27 -0600

    allow host

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,17 @@
 import { defineConfig } from "astro/config";
+import node from "@astrojs/node";
 
 export default defineConfig({
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
   vite: {
     optimizeDeps: {
       exclude: ["swagger-jsdoc"],
     },
     server: {
       host: true,
-      allowedHosts: ["localhost", "test.test"],
     },
   },
 });
